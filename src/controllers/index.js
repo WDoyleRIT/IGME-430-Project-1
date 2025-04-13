13775f4847ef0e661bf2f6fc8e34d5d044309638
Will Doyle <wbd6359@g.rit.edu>
2025-04-13 15:03:46 -0400

    GET Methods Implemented

--- src/controllers/index.js
+++ src/controllers/index.js
@@ -1,4 +1,6 @@
+const { text } = require('body-parser');
 const fs = require('fs');
+const { get } = require('http');
 
 const pokedex = JSON.parse(fs.readFileSync(`${__dirname}/../../data/pokedex.json`));
 
@@ -6,28 +8,59 @@ const getIndex = (req, res) => {
     res.render('index');
 };
 
-const getPokemon = (req, res) => {
-    let results = pokedex;
-  
-    if (req.query.id) {
-      const id = parseInt(req.query.id, 10);
-      results = results.filter((pokemon) => pokemon.id === id);
-    }
+const getPokemonByID = (req, res) => {
+  let results = pokedex;
 
-    if (req.query.name) {
-      const lowerName = req.query.name.toLowerCase();
-      results = results.filter((pokemon) => pokemon.name.toLowerCase().includes(lowerName));
-    }
-  
-    if (req.query.type) {
-      results = results.filter((pokemon) => pokemon.type.includes(req.query.type));
-    }
 
-    res.json(results)
+  if(req.query.id) {
+    const id = parseInt(req.query.id, 10);
+    results = results.filter((pokemon) => pokemon.id === id);
+  }
+  
+  console.log(results);
+  return res.render('index', {
+    search1: results,
+  });
 };
 
+const getPokemonByName = (req, res) => {
+let results = pokedex;
+
+let newName = req.query.name.toLowerCase();
+newName = newName.charAt(0).toUpperCase() + newName.slice(1);
+console.log(newName);
+results = results.filter((pokemon) => pokemon.name.includes(newName));
+
+return res.render('index', {
+  search2: results,
+});
+}
+
+const getPokemonByType = (req, res) => {
+let results = pokedex;
+
+results = results.filter((pokemon) => pokemon.type.includes(req.query.type));
+
+console.log(results);
+
+return res.render('index', {
+  search3: results,
+});
+}
+
+const getPokemonByWeakness = (req, res) => {
+  let results = pokedex;
+
+  results = results.filter((pokemon) => pokemon.weaknesses.includes(req.query.weaknesses));
+  
+
+  return res.render('index', {
+    search4: results,
+  });
+}
+
 const getData = (req, res) => {
-    res.json(pokedex.filter((x) => x.title.toLowerCase() === 'Pokemon'));
+    res.json(pokedex);
 };
 
 const notFound = (req, res) => {
@@ -38,7 +71,10 @@ const notFound = (req, res) => {
 
 module.exports = {
     getIndex,
-    getPokemon,
+    getPokemonByID,
+    getPokemonByName,
+    getPokemonByType,
+    getPokemonByWeakness,
     notFound,
     getData,
 };
--- src/router.js
+++ src/router.js
@@ -3,7 +3,10 @@ const controllers = require('./controllers');
 const router = (app) => {
     app.get('/', controllers.getIndex);
 
-    app.get('/getPokemon', controllers.getPokemon);
+    app.get('/getPokemonById', controllers.getPokemonByID);
+    app.get('/getPokemonByName', controllers.getPokemonByName);
+    app.get('/getPokemonByType', controllers.getPokemonByType);
+    app.get('/getPokemonByWeakness', controllers.getPokemonByWeakness);
     app.get('/getData', controllers.getData);
   
     // V This doesn't work anymore lol!
